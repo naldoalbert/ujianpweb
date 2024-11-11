e460107560472e291de3aa25a8e0c15556e1def7
Albert Gulo <albertjgulo@gmail.com>
2024-11-11 18:37:59 +0700

    keempat

--- ujian-project/src/App.jsx
+++ ujian-project/src/App.jsx
@@ -1,5 +1,31 @@
+// import React from 'react';
+// import Footer from './sections/Footer';
+// import Testimonials from './sections/Testimonials';
+// import Shop from './sections/Shop';
+// import Category from './sections/Category';
+// import Features from './sections/Features';
+// import Hero from './sections/Hero';
+// import Nav from './sections/Nav';
+
+// export default function App() {
+//   return (
+//     <div className="relative">
+//       <Nav />
+//       <Hero />
+//       <Features />
+//       <Category />
+//       <Shop />
+//       <Testimonials />
+//       <Footer />
+//     </div>
+
+//   );
+// }
+
+
+
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './sections/Footer';
 import Testimonials from './sections/Testimonials';
 import Shop from './sections/Shop';
@@ -7,22 +33,24 @@ import Category from './sections/Category';
 import Features from './sections/Features';
 import Hero from './sections/Hero';
 import Nav from './sections/Nav';
-// import BuyNowButton from './components/BuyNowButton';
-// import BuyPages from './pages/BuyPages';
+import BuyerDataPage from './pages/BuyerDataPage'; // Import your new page
 
 export default function App() {
   return (
-    // <Router>
-    <div className="relative">
-      <Nav />
-      <Hero />
-      <Features />
-      <Category />
-      <Shop />
-      <Testimonials />
-      <Footer />
-      {/* <BuyNowButton /> */}
-    </div>
-
+    <Router>
+      <div className="relative">
+        <Nav />
+        <Hero />
+        <Features />
+        <Category />
+        <Shop />
+        <Testimonials />
+        <Footer />
+        {/* Add Routes */}
+        <Routes>
+          <Route path="/buyer-data" element={<BuyerDataPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
--- ujian-project/src/components/ShopCard.jsx
+++ ujian-project/src/components/ShopCard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import { RiStarFill, RiStarLine } from 'react-icons/ri'
+// import React from 'react'
+// import { RiStarFill, RiStarLine } from 'react-icons/ri'
+
+// const ShopCard = ({ imgURL, title, price }) => {
+//     return (
+//         <div className="shop-card relative flex flex-col w-fullmax-sm:w-full">
+//             <img src={imgURL} alt={title} />
+//             <h4 className='mt-2 text-[1rem] font-[600] capitalize'>{title}</h4>
+//             <p className='mt-2 font-[500] text-[.88rem] text-[#777] leading-normal'>{price}</p>
+//             <div className="rating flex text-[#717fe0] mt-1">
+//                 <RiStarFill />
+//                 <RiStarFill />
+//                 <RiStarFill />
+//                 <RiStarFill />
+//                 <RiStarLine />
+//             </div>
+//             <div className="flex gap-2 mt-4">
+//                 <button className='text-[.7rem] uppercase border-solid border-[#777] text-black p-2 hover:bg-black hover:border-black hover:text-white mr-1' >Buy Now</button>
+//                 <button className='text-[.7rem] uppercase border-solid border-[#777] text-black p-2 hover:bg-black hover:border-black hover:text-white' >Add to Cart +</button>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default ShopCard
+
+
+import React from 'react';
+import { RiStarFill, RiStarLine } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const ShopCard = ({ imgURL, title, price }) => {
+    const navigate = useNavigate(); // Initialize useNavigate
+
+    const handleBuyNow = () => {
+        navigate('/buyer-data'); // Navigate to the buyer data page
+    };
+
     return (
-        <div className="shop-card relative flex flex-col w-fullmax-sm:w-full">
+        <div className="shop-card relative flex flex-col w-full max-sm:w-full">
             <img src={imgURL} alt={title} />
             <h4 className='mt-2 text-[1rem] font-[600] capitalize'>{title}</h4>
             <p className='mt-2 font-[500] text-[.88rem] text-[#777] leading-normal'>{price}</p>
@@ -15,11 +49,11 @@ const ShopCard = ({ imgURL, title, price }) => {
                 <RiStarLine />
             </div>
             <div className="flex gap-2 mt-4">
-                <button className='text-[.7rem] uppercase border-solid border-[#777] text-black p-2 hover:bg-black hover:border-black hover:text-white mr-1' >Buy Now</button>
-                <button className='text-[.7rem] uppercase border-solid border-[#777] text-black p-2 hover:bg-black hover:border-black hover:text-white' >Add to Cart +</button>
+                <button onClick={handleBuyNow} className="bg-blue-500 text-white py-2 px-4 rounded">Buy Now</button>
+                <button className="bg-gray-300 text-black py-2 px-4 rounded">Add to Cart</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ShopCard
+export default ShopCard;
